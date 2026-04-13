c9f1c78b26279d156458a4d90f72d62004aea9a7
BelizarioJV <joaovictor.fera@hotmail.com>
2026-04-13 19:29:10 -0300

    (update) - refatorei as divs e coloquei um formulario , pensei melhor no projeto e decidi buscar passagens ou ja esquematizar toda a viagem com os dados de entrada , vou finalizar a funçao handleSubmit e depois pensar na logica para a outra parte do  projeto

--- src/App.tsx
+++ src/App.tsx
@@ -5,19 +5,28 @@ import {
   MapPin,
   User,
   Settings2,
+  X,
 } from "lucide-react";
 import { useState } from "react";
 
 function App() {
   const [showInviteEmail, setShowInviteEmail] = useState(false);
   const [showDialog, setShowDialog] = useState(false);
+  const [guests, setShowGuests] = useState<string[]>([]);
+  const [newGuest, setNewGuest] = useState<string>("");
+
+  const handleSubmit = () => {
+    // Lógica para enviar os dados da viagem e convidados para o backend
+    console.log("Viagem planejada com os seguintes convidados:", guests);
+  };
 
   return (
     <>
       <main className="h-screen flex flex-col items-center justify-center gap-6 ">
-        <div
+        <form
           className="flex flex-col items-center bg-zinc-600 rounded-lg 
-              shadow-lg shadow-black/80 border border-white/5 p-10">
+              shadow-lg shadow-black/80 border border-white/5 p-10"
+          onSubmit={(e) => e.preventDefault()}>
           {/* Texto e logo */}
           <div className="flex min-w-xl items-center justify-center">
             <PlaneTakeoff size={52} color="#b6e372" />
@@ -50,13 +59,13 @@ function App() {
               {showInviteEmail ? (
                 <button
                   onClick={() => setShowInviteEmail(!showInviteEmail)}
-                  className="bg-zinc-500 text-amber-100rounded-lg p-5 font-medium flex items-center ">
+                  className="bg-zinc-500 text-white rounded-lg shadow-lg shadow-black/80 p-5 font-medium flex items-center ">
                   Alterar local/data <Settings2 />
                 </button>
               ) : (
                 <button
                   onClick={() => setShowInviteEmail(!showInviteEmail)}
-                  className="bg-lime-300 text-lime-950 rounded-lg p-5 font-medium flex items-center ">
+                  className="bg-lime-300 text-lime-950 rounded-lg shadow-lg shadow-black/80 p-5 font-medium flex items-center ">
                   Continuar <ArrowRight />
                 </button>
               )}
@@ -64,43 +73,83 @@ function App() {
 
             {/* Rederizaçao condicional */}
             {showInviteEmail && (
-              <div className="flex flex-row justify-between gap-5">
-                <div className=" bg-zinc-500 rounded-lg p-5 shadow-lg shadow-black/80">
-                  <User />
-                  <input
-                    className="p-2 mt-2 outline-0"
-                    type="text"
-                    placeholder="Quem estara na viagem ? "
-                  />
-                </div>
-                <button
-                  onClick={() => setShowDialog(!showDialog)}
-                  className="bg-lime-300 text-lime-950 rounded-lg p-5 font-medium flex items-center ">
-                  Confirmar viagem <ArrowRight />
-                </button>
+              <div className="w-full">
+                {guests.length <= 0 ? (
+                  <div className=" flex flex-row justify-between bg-zinc-500 rounded-lg p-5 shadow-lg shadow-black/80 w-full">
+                    <div>
+                      <User />
+                      <p>Quem estara na viagem?</p>
+                    </div>
+                    <button
+                      onClick={() => setShowDialog(!showDialog)}
+                      className="bg-lime-300 text-lime-950 rounded-lg p-5 font-medium flex items-center ">
+                      <ArrowRight />
+                    </button>
+                  </div>
+                ) : (
+                  <div className=" flex flex-row justify-between bg-zinc-500 rounded-lg p-5 shadow-lg shadow-black/80 w-full">
+                    <div>
+                      <User />
+                      <p>Confirmar viagem</p>
+                    </div>
+                    <button
+                      onSubmit={handleSubmit}
+                      className="bg-lime-300 text-lime-950 rounded-lg p-5 font-medium flex items-center ">
+                      <ArrowRight />
+                    </button>
+                  </div>
+                )}
               </div>
             )}
           </div>
-        </div>
+        </form>
 
         {/* Dialog */}
         {showDialog && (
           <div className="fixed inset-0 bg-black/60 flex items-center justify-center">
-            <div
-              className="w-160 rounded-lg p-6 shadow-black/50 bg-zinc-900 flex flex-col gap-3"
-              onClick={() => setShowDialog(false)}>
-              <h4>Selecione convidados</h4>
+            <div className="w-160 rounded-lg p-6 shadow-black/50 bg-zinc-600 flex flex-col gap-3">
+              <div className="flex flex-row justify-between w-full">
+                <h3 className="font-lg font-semibold">Selecione convidados</h3>
+                <X onClick={() => setShowDialog(!showDialog)} />
+              </div>
               <p>
                 Os convidados irao receber e-mails para confirmar a participação
                 na viagem
               </p>
-              <div>{}</div>
+              <div className="flex flex-wrap gap-2">
+                {guests.map((guest) => (
+                  <div
+                    key={guest}
+                    className="rounded-lg shadow-black/80 bg-zinc-900 flex flex-row p-4 gap-3 w-fit">
+                    <span>{guest}</span>
+                    <button className="bg-lime-300 text-lime-950 shadow-lgshadow-black/80 rounded-lg ">
+                      <X
+                        onClick={() =>
+                          setShowGuests(guests.filter((g) => g != guest))
+                        }
+                      />
+                    </button>
+                  </div>
+                ))}
+              </div>
               <hr />
               <input
-                className="w-full border rounded grow-0"
+                className="w-full border rounded p-4"
                 type="text"
                 placeholder="Digite o email do convidado"
+                value={newGuest}
+                onChange={(e) => setNewGuest(e.target.value)}
               />
+              <button
+                className="bg-lime-300 text-lime-950 rounded-lg p-5 font-medium justify-c "
+                onClick={() => {
+                  if (newGuest.trim() !== "") {
+                    setShowGuests([...guests, newGuest]);
+                    setNewGuest("");
+                  }
+                }}>
+                Adicionar convidado
+              </button>
             </div>
           </div>
         )}
